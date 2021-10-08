04cd8a4ec26e76d35cf315c8d25e33b01d765b83
Shivam Pandey <49595915+shivampandeypandey@users.noreply.github.com>
2021-10-08 17:14:04 +0530

    Please use your aws s3 credentials

--- backend/Utils/config.js
+++ backend/Utils/config.js
@@ -4,10 +4,11 @@ const config = {
     // frontendURL: "http://13.57.9.43:3000",
     frontendURL: "http://localhost:3000",
     mongoDB: 'mongodb+srv://akhil:abcd1234@cluster0.lcwnm.mongodb.net/shivam?retryWrites=true&w=majority',
-    AWS_ACCESS_KEY_ID: "AKIAQGNL2AHHR4EH2H7O",
-    AWS_SECRET_ACCESS_KEY: "afve2O1qli3++z82xTEWKX/WV3C8EbHAem8QJJhM",
-    AWS_REGION: "ap-south-1",
-    AWS_BUCKET_NAME: "testappseller",
+    AWS_ACCESS_KEY_ID: "Your Cred",
+    AWS_SECRET_ACCESS_KEY: "Your cred",
+    AWS_REGION: "Your cred",
+   
+    AWS_BUCKET_NAME:"Your cred",
 };
 
 
