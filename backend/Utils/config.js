0d6ef55e1d0a08b03589f655def808e8c3b224a0
Shivam Pandey <49595915+shivampandeypandey@users.noreply.github.com>
2021-10-08 17:10:56 +0530

    Add files via upload

---
+++ backend/Utils/config.js
@@ -0,0 +1,16 @@
+const config = {
+
+    secret: "cmpe202_secret_key",
+    // frontendURL: "http://13.57.9.43:3000",
+    frontendURL: "http://localhost:3000",
+    mongoDB: 'mongodb+srv://akhil:abcd1234@cluster0.lcwnm.mongodb.net/shivam?retryWrites=true&w=majority',
+    AWS_ACCESS_KEY_ID: "AKIAQGNL2AHHR4EH2H7O",
+    AWS_SECRET_ACCESS_KEY: "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",
+    AWS_REGION: "ap-south-1",
+    AWS_BUCKET_NAME: "testappseller",
+};
+
+
+
+module.exports = config;
+
